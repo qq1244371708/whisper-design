7b1c4ebbb2e4498e21295e2b2011002e47a8514e
breeze <gaojun@whalepms.com>
2025-07-02 16:18:44 +0800

    feat(.location):

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "demoWhisperDesign",
       formats: ["es", "cjs"],
-      fileName: (format) => `index.${format === "es" ? "mjs" : "js"}`,
+      fileName: (format) => `[name].${format === "es" ? "mjs" : "js"}`,
     },
     rollupOptions: {
       external: ["react", "react-dom"],
@@ -27,7 +27,25 @@ export default defineConfig({
           react: "React",
           "react-dom": "ReactDOM",
         },
-        assetFileNames: "assets/[name][extname]",
+        manualChunks: (id: string) => {
+          if (id.includes("src/components/")) {
+            const component = id.split("src/components/")[1].split("/")[0];
+            return `components/${component}/index`;
+          }
+        },
+        assetFileNames: (assetInfo: { name?: string }) => {
+          if (assetInfo.name?.endsWith(".css")) {
+            const match = assetInfo.name.match(
+              /components\/([^/]+)\/index\.css/
+            );
+            if (match) {
+              const component = match[1];
+              return `components/${component}/style/index.css`;
+            }
+            return `assets/[name]-[hash][extname]`;
+          }
+          return "assets/[name]-[hash][extname]";
+        },
       },
     },
     cssCodeSplit: true,
